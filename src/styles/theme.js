3542bcff3d753ea7d7c8001a5e29abedcea583d4
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-09-28 12:26:51 -0300

    created styles/theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,54 @@
+export default {
+
+  COLORS: {
+    BG: "#000A0F",
+    MAIN_T: "#E1E1E6",
+    MAIN_SPAN: "#82F3FF",
+
+    NAV_H1: "#FFFFFF",
+    NAV_ICON: "#065E7C",
+    NAV_SPAN: "#82F3FF",
+
+
+    MENU_BUTTON_LIST_B: "#192227",
+
+
+    TAG_BG: "#192227",
+    TAG_T: "#FFFFFF",
+
+    TEXT_BUTTON: "#E1E1E6",
+    ICON_BUTTON: "#FFFFFF",
+
+
+    BUTTON_BG: "#750310",
+    BUTTON_ADMIN_BG: "#AB4D55",
+    BUTTON_DEL_ADMIN_BG: "#0D161B",
+    BUTTON_T: "#FFFFFF",
+
+
+    LABEL: "#C4C4CC",
+    INPUT_AUTH_BG:  "#0D1D25",
+    INPUT_ADMIN_BG: "#0D161B",
+    INPUT_T: "#7C7C8A",
+    INPUT_ICON: "#C4C4CC",
+
+    INPUT_SELECT_BG:  "#0D1D25",
+    INPUT_SELECT_T: "#C4C4CC",
+
+    ITEM_NEW_T__BD: "#7C7C8A",
+    ITEM_NEW_BG: "transparent",
+    ITEM_T: "#FFFFFF",
+    ITEM_BG: "#76797B",
+
+
+    FOOTER_BG: "#00111A",
+    FOOTER_BRAND: "#4D585E",
+    FOOTER_RIGHTS: "#FFFAF1",
+  },
+
+  FONT_FAMILY: {
+    BODY: "'Poppins', sans-serif",
+    BRAND: "'Roboto', sans-serif",
+    FOOTER_RIGHTS: "'DM Sans', sans-serif",
+  },
+}
