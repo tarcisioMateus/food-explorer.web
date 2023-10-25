9313a767d8a0b54002a7bbccfcb9673665160b5e
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-25 16:40:53 -0300

    created src/routes/customer.routes.jsx

---
+++ src/routes/customer.routes.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom"
+
+import { Home }  from "../pages/Home"
+import { Dish }  from "../pages/Dish"
+
+export function CustomerRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/dish/:id" element={<Dish/>} />
+    </Routes>
+  )
+}
