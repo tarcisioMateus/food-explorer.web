4b4bc4a6931ec934ed0650b57995bb02f2c2a93d
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-25 16:45:41 -0300

    created src/routes/index.jsx

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom"
+
+import { AdminRoutes } from "./admin.routes"
+import { CustomerRoutes } from "./customer.routes"
+import { AuthRoutes } from "./auth.routes"
+
+export function Routes() {
+
+  return (
+    <BrowserRouter>
+      <AuthRoutes/>
+    </BrowserRouter>
+  )
+}
