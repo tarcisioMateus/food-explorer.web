ccdeb79cbec142f0ad8b58ab08f7c7c3aea88ca5
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-26 14:05:18 -0300

    created src/hooks/auth.jsx - signIn()

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,12 +1,40 @@
-import { createContext, useContext } from 'react'
-
+import { createContext, useContext, useState, useEffect } from 'react'
+import { api } from '../services'
 
 const AuthContext = createContext({})
 
 function AuthProvider( { children } ) {
+  const [user, setUser] = useState({})
+
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post(
+        '/sessions', 
+        { email, password }, 
+        { withCredentials: true }
+      )
+      const { user } = response.data
+
+      localStorage.setItem('@foodExplorer:user', JSON.stringify(user))
+      setUser( user )
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert('unable to sign in right now, please try again later!')
+      }
+    }
+  }
+  
+  useEffect(() => {
+    const user = localStorage.getItem('@foodExplorer:user')
+
+    setUser(JSON.parse(user))
+  }, [])
 
   return (
-    <AuthContext.Provider value={{ }}>
+    <AuthContext.Provider value={{ signIn, user}}>
       {children}
     </AuthContext.Provider>
   )
