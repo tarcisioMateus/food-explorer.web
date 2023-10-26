b4ff9a9b5164264c0231918ff0176578c27384be
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-26 16:47:59 -0300

    implemented admin variation on src/components/Brand

--- src/components/Brand/index.jsx
+++ src/components/Brand/index.jsx
@@ -1,15 +1,27 @@
 import BrandSvg from '../../assets/brand.svg'
+import { USER_ROLE } from '../../utils/roles'
+import { useAuth } from '../../hooks/auth'
 
 import { Container } from "./styles"
 
 export function Brand({ className, ...rest }) {
+  const { user } = useAuth()
+
   return (
     <Container {...rest} className={`brand ${className}`}>
       <img
         src = {BrandSvg}
         alt='light blue hexagon'
       />
-      <h1>food explorer</h1>
+
+      <div>
+        <h1>food explorer</h1>
+
+        {
+          ( user && [USER_ROLE.ADMIN].includes(user.role) ) &&
+          <p>admin</p>
+        }
+      </div>
     </Container>
   )
 }
--- src/components/Brand/styles.js
+++ src/components/Brand/styles.js
@@ -10,11 +10,25 @@ export const Container = styled.header`
     margin-right: 1rem;
   }
 
-  >h1 {
-    font-family: ${ ({theme}) => theme.FONT_FAMILY.BRAND};
-    color: ${ ({theme}) => theme.COLORS.H1};
-    font-weight: 700;
-    font-size: 3.6rem;
+  >div {
+    display: flex;
+    align-items: center;
+
+    >h1 {
+      font-family: ${ ({theme}) => theme.FONT_FAMILY.BRAND};
+      color: ${ ({theme}) => theme.COLORS.H1};
+      font-weight: 700;
+      font-size: 3.6rem;
+    }
+
+    >p {
+      font-family: ${ ({theme}) => theme.FONT_FAMILY.BRAND};
+      color: ${ ({theme}) => theme.COLORS.SPAN};
+      font-size: 1.2rem;
+      line-height: 100%;
+      font-weight: 400;
+      margin-left: .8rem;
+    }
   }
 
   &.nav {
@@ -24,8 +38,11 @@ export const Container = styled.header`
       height: clamp(2.4rem, .1rem + 3.7vw, 3rem);
     }
 
-    >h1 {
-      font-size: clamp(2.1rem, .3rem + 3.5vw, 2.4rem);
+    >div {
+
+      >h1 {
+        font-size: clamp(2.1rem, .3rem + 3.5vw, 2.4rem);
+      }
     }
   }
 
@@ -39,9 +56,16 @@ export const Container = styled.header`
       filter: grayscale(.8);
     }
 
-    >h1 {
-      font-size: clamp(1.5rem, .2rem + 3.5vw, 2.4rem);
-      color: ${ ({theme}) => theme.COLORS.FOOTER_BRAND};
+    >div {
+
+      >h1 {
+        font-size: clamp(1.5rem, .2rem + 3.5vw, 2.4rem);
+        color: ${ ({theme}) => theme.COLORS.FOOTER_BRAND};
+      }
+
+      >p {
+        display: none;
+      }
     }
   }
 
@@ -54,8 +78,19 @@ export const Container = styled.header`
       height: clamp(4.4rem, .4rem + 4.6vw, 4.75rem);
     }
 
-    >h1 {
-      font-size: clamp(3.6rem, 3rem + 1vw, 4.2rem);
+    >div {
+      position: relative;
+      >h1 {
+        font-size: clamp(3.6rem, 3rem + 1vw, 4.2rem);
+      }
+
+      >p {
+        line-height: 160%;
+        position: absolute;
+        z-index: 10;
+        right: 0;
+        bottom: -1.8rem;
+      }
     }
 
     &.nav {
