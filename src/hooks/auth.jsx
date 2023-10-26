be4f8c8ce5d75a88d54637e36902d366e6af1eee
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-26 13:57:24 -0300

    created src/hooks/auth.jsx

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext } from 'react'
+
+
+const AuthContext = createContext({})
+
+function AuthProvider( { children } ) {
+
+  return (
+    <AuthContext.Provider value={{ }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth () {
+  const context = useContext(AuthContext)
+  return context
+}
+
+export { AuthProvider, useAuth }
