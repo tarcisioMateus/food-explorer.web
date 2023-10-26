300b62a260cb31b9e91efd04db0918c53dba595b
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-26 15:09:23 -0300

    updated src/routes/index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,27 @@
 import { BrowserRouter } from "react-router-dom"
+import { useAuth } from '../hooks/auth'
+import { USER_ROLE } from '../utils/roles'
 
 import { AdminRoutes } from "./admin.routes"
 import { CustomerRoutes } from "./customer.routes"
 import { AuthRoutes } from "./auth.routes"
 
 export function Routes() {
+  const { user } = useAuth()
+
+  function AccessRoute() {
+    switch (user.role) {
+      case USER_ROLE.ADMIN:
+        return <AdminRoutes/>
+      default:
+        return <CustomerRoutes/>
+    }
+  }
+
 
   return (
     <BrowserRouter>
-      <AuthRoutes/>
+      {user ? <AccessRoute/> : <AuthRoutes/>}
     </BrowserRouter>
   )
 }
